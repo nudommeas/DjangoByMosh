195b736a29b6e2a54ca5396aae6a4d6e0589e16a
nudommeas <measthannudom@gmail.com>
2025-12-16 10:08:00 +0700

    Choice Fields

--- store/models.py
+++ store/models.py
@@ -6,11 +6,34 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField()
     inventory = models.IntegerField()
-    last_update = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True) #Automatically updates on each save
 
 class Customer(models.Model):
+    MEMBER_BRONZE = 'B'
+    MEMBER_SILVER = 'S'
+    MEMBER_GOLD = 'G'
+
+    MEMBER_CHOICES= [
+        (MEMBER_BRONZE, 'Bronze'),
+        (MEMBER_SILVER, 'Silver'),
+        (MEMBER_GOLD, 'Gold')
+    ]
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255,unique=True)
     phone = models.CharField()
     birth_data = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBER_CHOICES, default=MEMBER_BRONZE)
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETED = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS = [
+        (PAYMENT_STATUS_PENDING, 'PENDING'),
+        (PAYMENT_STATUS_COMPLETED, 'COMPLETED'),
+        (PAYMENT_STATUS_FAILED, 'FAILED')
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True) #Automatically sets on creation
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING)
