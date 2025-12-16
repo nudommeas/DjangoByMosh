a80c5a3bb23c40e074f221bb741471e136e6d302
nudommeas <measthannudom@gmail.com>
2025-12-16 09:56:15 +0700

    Creating Models

---
+++ store/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField()
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255,unique=True)
+    phone = models.CharField()
+    birth_data = models.DateField(null=True)
