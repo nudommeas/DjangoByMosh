10c3693bb381d0f8e2d0928b3f3718f4fcdad9dd
nudommeas <measthannudom@gmail.com>
2025-12-16 12:53:51 +0700

    Creating Migrations

--- store/models.py
+++ store/models.py
@@ -9,8 +9,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True) #Automatically updates on each save
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)# collection is parent class
--- tags/models.py
+++ tags/models.py
@@ -14,10 +14,10 @@ class TaggedItem(models.Model):
     #ID using the ID, we can find the record
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey(content_type, object_id)
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 class LikeItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey(content_type, object_id)
+    content_object = GenericForeignKey('content_type', 'object_id')
