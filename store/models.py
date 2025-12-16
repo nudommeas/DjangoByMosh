c4252afaf3e2bb74d574b142e19d22625bae5dac
nudommeas <measthannudom@gmail.com>
2025-12-16 11:53:09 +0700

    Creating OneTomany Relationships

--- store/models.py
+++ store/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField()
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True) #Automatically updates on each save
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)# collection is parent class
 
 class Customer(models.Model):
     MEMBER_BRONZE = 'B'
@@ -37,9 +40,24 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True) #Automatically sets on creation
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.DecimalField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True) #Primary key is not allowed to duplicate address
-    
+    #customer is the parent class
+
+class Cart(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
