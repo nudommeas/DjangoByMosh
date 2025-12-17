d476b9045d8dd5bfcf060b245161ad4690434c8f
nudommeas <measthannudom@gmail.com>
2025-12-17 17:11:36 +0700

    Complex Lookups using Q Objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 from store.models import Collection
 # Create your views here.
 
+# __icontains 
+# __contains --> lookup type
+# __range --> lookup type
+# __year 
+
 
 def index(request):
-    queryset = Product.objects.filter(unit_price__range=(20,30))
+    queryset = Product.objects.filter(Q(inventory__lt=10) & Q(unit_price__lt=20))
     return render(request, 'hello.html', {'name': 'Nudom', 'products': list(queryset)})   
