b4c07f95dbde33f7e537aba9e88416f18f560530
nudommeas <measthannudom@gmail.com>
2025-12-23 21:01:16 +0700

    Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
-from store.models import Product
-from store.models import Collection
+from store.models import Product, Collection, Order
+
 # Create your views here.
 
 
 
 def index(request):
-    queryset = Product.objects.values('id', 'title', 'collection__title')
+    queryset = Product.objects.prefetch_related('promotion').select_related('collection')[:5]
+    # queryset = Order.objects.select_related('customer').order_by('-placed_at')[:5]
     return render(request, 'hello.html', {'name': 'Nudom', 'products': list(queryset)})   
