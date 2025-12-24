2f058771501ca3ff681ca74ca1090e5ff08ad2a9
nudommeas <measthannudom@gmail.com>
2025-12-24 13:33:03 +0700

    Adding Computed Columns

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,15 @@ from . import models
 # Register your models here.
 @admin.register(models.Product) #registered decorator
 class ProductAdmin(admin.ModelAdmin): #Class ModelAdmin
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product): #computed columns
+        if product.inventory < 10:
+            return 'LOW'
+        return 'OK'
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
