3b1e128faa1fbb7fcffd7d93dc29e864bf21a3bd
nudommeas <measthannudom@gmail.com>
2025-12-26 23:27:56 +0700

    Class-based Views

---
+++ AdvancedAPI.md
@@ -0,0 +1,4 @@
+**Class-based View**
+--> Using `APIVIEW`
+--> the incoming request is automatically sent to one of these methods, depending on its method.
+--> APIView handler method such as .get() , .post() , .put() , .patch() , .delete()
--- store/views.py
+++ store/views.py
@@ -3,36 +3,39 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 from django.db.models import Count
 # Create your views here.
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+
+class ProductList(APIView):
+    def get(self, request):
         product = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(product, many=True, context={'request': request}) #many=True tells the serializer to expect an iterable (like a queryset) and to serialize each item within it
         return Response(serializer.data)
-    elif request.method == 'POST':
+    
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id) #get the existing users data in the datase
-    if request.method == 'GET':
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id) #get the existing users data in the datase
         serializer = ProductSerializer(product)#this serializer will convert product object to a dictionary
         return Response(serializer.data)
-    elif request.method == 'PUT':
+    def put(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
         serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == 'DELETE':   
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request , id):
+        product = get_object_or_404(Product, pk=id)
         product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({'message': 'The product has been removed'}, status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def collection_list(request):
